3a10c3e03fd5954fb7faefcdcd18977304634632
Rebecca Roth <EclecticTurtleStudio@gmail.com>
2021-09-08 20:39:59 -0500

    tried to process form with xhr... not successful

--- frontend/login.js
+++ frontend/login.js
@@ -1,22 +1,24 @@
 
+
 function submitLogIn(){
 
-    var xhr = new XMLHttpRequest();
-    var url = 'http://localhost:8000/login';
     var email = document.getElementById("email");
     var password = document.getElementById("password");
+
+    var xhr = new XMLHttpRequest();
+    var url = "http://localhost:8000/login";
+
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            var json = JSON.parse(xhr.responseText);
-            console.log(json.email + ", " + json.password);
+            console.log("it worked");
         } else {
-            console.log("not working");
+            console.log("you suck");
         }
     };
-    var userInfo = JSON.stringify({"email": email, "password": password});
-    xhr.send(userInfo);
+//    var data = JSON.stringify({"email": email, "password": password});
+//    xhr.send(data);
 
 };
 
