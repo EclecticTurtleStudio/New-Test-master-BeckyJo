923027e672abffebb47fd3a0d9b1fc5fa8a77f26
Rebecca Roth <EclecticTurtleStudio@gmail.com>
2021-09-22 17:16:19 -0500

    2 ways to submit form... neither work 100%

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,24 @@
+serverAuth =
 
+function showMovieDatabase(serverAuth){
+
+//This is supposed to take the JWT and send it to the server to allow authorization to see the database list.
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function() {
+      if(this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    });
+
+    xhr.open("GET", "http://localhost:8000/movies");
+    xhr.setRequestHeader("Authorization", "Bearer " + serverAuth);
+
+    xhr.send();
+
+    console.log("pizza");
+
+}
 
--- frontend/login.js
+++ frontend/login.js
@@ -1,25 +1,39 @@
 
-
 function submitLogIn(){
 
-    var user = document.getElementById("email");
-    var pswd = document.getElementById("password");
+// This function will eventually send the form to /login and return the accessToken!
+
+    var loginForm = document.getElementById('loginForm');
+    var formData = new FormData(loginForm);
+
+    console.log(formData);
+
+    var user = formData.get(email);
+    var pswd = formData.get(password);
+
+    console.log(user + " & " + pswd);
+
+    var data = JSON.stringify({"email": user.value, "password": pswd.value});
 
     var xhr = new XMLHttpRequest();
     var url = "http://localhost:8000/login";
 
+    xhr.addEventListener("readystatechange", function() {
+        if(url.readyState === 4) {
+            console.log(url.response);
+        }
+    });
+
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log("it worked");
-        }
-    }
 
-    var data = JSON.stringify({"email": user, "password": pswd});
     xhr.send(data);
 
+    var serverAuth = JSON.accessToken;
 
+    console.log(serverAuth);
+
+    return serverAuth;
 
 };
 
