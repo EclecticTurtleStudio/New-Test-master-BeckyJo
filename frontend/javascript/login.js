de637d30c1a691b606de1ae460e70e020f98a28e
Rebecca Roth <EclecticTurtleStudio@gmail.com>
2021-09-25 23:11:11 -0500

    The form elements are being got!! WAHOO!!

--- frontend/javascript/login.js
+++ frontend/javascript/login.js
@@ -6,29 +6,33 @@
 
 function submitLogIn(){
 
-    var user = document.getElementById("email").value;
-    var pswd = document.getElementById("password").value;
+    var user = document.querySelector("input[name=email]").value;
+    var pswd = document.querySelector("input[name=password]").value;
+
+    //email and password are being obtained from HTML form!! WAHOO!!
 
     var data = JSON.stringify({
       "email": user,
       "password": pswd
     });
 
+    var url = "http://localhost:8000/login";
+
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
 
     xhr.addEventListener("readystatechange", function() {
-      if(this.readyState === 4) {
-        console.log(this.responseText);
+      if(url.readyState === 4) {
+        console.log(responseText);
       }
     });
 
-    xhr.open("POST", "http://localhost:8000/login");
+    xhr.open("POST", url);
     xhr.setRequestHeader("Content-Type", "application/json");
 
     xhr.send(data);
 
-
+    console.log(response.json);
 
 };
 
